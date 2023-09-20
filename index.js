c1e321998338c0873349ae97b192e2355533d265
Richard <richacosta06@gmail.com>
2023-09-19 19:41:36 -0500

    Cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import  express  from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import conectarDB from "./config/db.js";
 import usuarioRoutes from './routes/usuarioRoutes.js'
 import exhibicionRoutes from './routes/exhibicionRoutes.js'
-import productoRoutes from './routes/productoRoutes.js'
-import pedidoRoutes from './routes/pedidoRoutes.js'
+import productoRoutes from './routes/productoRoutes.js';
+import pedidoRoutes from './routes/pedidoRoutes.js';
 
 
 const app = express();
@@ -13,6 +14,21 @@ app.use(express.json());
 dotenv.config();
 
 conectarDB();
+
+//Configurar CORS
+// const whitelist = ["http://localhost:5173"];
+
+// const corsOptions = {
+//     origin: function(origin, callback){
+//         if(whitelist.includes(origin)){
+//            callback(null, true);
+//         }else{
+//             callback(new Error("Error de Cors"))
+//         }
+//     },
+// };
+
+app.use(cors());
 
 //Routing
 app.use("/api/usuarios", usuarioRoutes);
